4bdafc1d48dd747a49a21fce67dbc4c1f8823fb1
watanase <veltread@gmail.som>
2020-05-26 05:18:56 +0900

    task表示

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -9,15 +9,6 @@ class FolderController extends Controller
 {
     public function showCreateForm(Request $request)
     {
-        // フォルダモデルのインスタンスを作成する
-        $folder = new Folder();
-        // タイトルに入力値を代入する
-        $folder->title = $request->title;
-        // インスタンスの状態をデータベースに書き込む
-        $folder->save();
-
-        return redirect()->route('tasks.index', [
-            'id' => $folder->id,
-        ]);
+        return view('folders/create');
     }
 }
